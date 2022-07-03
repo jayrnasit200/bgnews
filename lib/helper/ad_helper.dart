9f8e56820fa935b2206e25a122f9e549d5d5ec51
Nasit Jay <72692535+jayrnasit200@users.noreply.github.com>
2022-07-03 18:06:23 +0530

    done

--- lib/helper/ad_helper.dart
+++ lib/helper/ad_helper.dart
@@ -4,8 +4,10 @@ class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
       return "ca-app-pub-3940256099942544/6300978111";
+      // return "ca-app-pub-4652444325266246/7054672320";
     } else if (Platform.isIOS) {
       return "ca-app-pub-3940256099942544/2934735716";
+      // return "ca-app-pub-4652444325266246/2057781922";
     } else {
       throw new UnsupportedError("Unsupported platform");
     }
@@ -14,8 +16,10 @@ class AdHelper {
   static String get interstitialAdUnitId {
     if (Platform.isAndroid) {
       return "ca-app-pub-3940256099942544/8691691433";
+      // return "ca-app-pub-4652444325266246/3377755960";
     } else if (Platform.isIOS) {
       return "ca-app-pub-3940256099942544/5135589807";
+      // return "ca-app-pub-4652444325266246/4089306053";
     } else {
       throw new UnsupportedError("Unsupported platform");
     }
